c3ec132581936fbc2ca02c1d14611a7dd1910696
Florian Müller <florian.mueller@shopgate.com>
2015-06-18 17:59:58 +0200

    collision detection

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,15 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: '\n\n'
+			},
+			dist: {
+				src: ['build/overlaps.js', 'build/image-mapper.js'],
+				dest: 'jquery.image-mapper.js'
+			}
+		},
 		uglify: {
 			options: {
 				sourceMap: true,
@@ -12,8 +21,11 @@ module.exports = function (grunt) {
 					drop_console: true,
 					drop_debugger: true
 				},
-				banner: '/* <%= pkg.title %> - v<%= pkg.version %>\n' +
+				banner: '/* <%= pkg.title %> v<%= pkg.version %> - <%= pkg.homepage %>\n' +
+				' * <%= pkg.description %>\n' +
+				' * \n' +
 				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+				' * Licensed under the <%= pkg.license %> license\n' +
 				' * <%= grunt.template.today("yyyy-mm-dd") %>\n' +
 				' */'
 			},
@@ -22,8 +34,12 @@ module.exports = function (grunt) {
 					'jquery.image-mapper.min.js': ['jquery.image-mapper.js']
 				}
 			}
+		},
+		watch: {
+			files: ['build/*.js'],
+			tasks: ['concat', 'uglify']
 		}
 	});
 
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['concat', 'uglify', 'watch']);
 };
---
+++ build/overlaps.js
@@ -0,0 +1,33 @@
+(function ($) {
+	"use strict";
+
+	$.fn.overlaps = function(obj, tolerance) {
+		var elems = {targets: [], hits:[]};
+		var tol = (typeof tolerance === "undefined") ? 1 : tolerance;
+
+		this.each(function() {
+			// Calculate the bounds
+			var bounds = $(this).offset();
+			bounds.right = bounds.left + $(this).outerWidth();
+			bounds.bottom = bounds.top + $(this).outerHeight();
+
+			// Calculate the compares
+			var compare = $(obj).offset();
+			compare.right = compare.left + $(obj).outerWidth();
+			compare.bottom = compare.top + $(obj).outerHeight();
+
+			if ( ! (
+				(compare.right+tol) < bounds.left ||
+				(compare.left+tol) > bounds.right ||
+				(compare.bottom+tol) < bounds.top ||
+				(compare.top+tol) > bounds.bottom)
+			) {
+				elems.targets.push(this);
+				elems.hits.push(obj);
+			}
+		});
+
+		return elems;
+	};
+
+})(jQuery);
